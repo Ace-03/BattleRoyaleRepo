1e4874d544a809fa601037b2aee79279879b0f60
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2023-10-04 11:11:30 -0700

    New force field (started Battle Royale++)

--- BattleRoyale/Assets/Scripts/ForceField.cs
+++ BattleRoyale/Assets/Scripts/ForceField.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class ForceField : MonoBehaviour
+public class ForceField : MonoBehaviourPun
 {
     public float shrinkWaitTime;
     public float shrinkAmount;
     public float shrinkDuration;
     public float minShrinkAmount;
-
+    public Vector3 targetCenterLocation;
+    public float centerLocationContstraint;
     public int playerDamage;
 
     private float lastShrinkEndTime;
@@ -27,6 +29,7 @@ public class ForceField : MonoBehaviour
         if (shrinking)
         {
             transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.one * targetDiameter, (shrinkAmount / shrinkDuration) * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetCenterLocation, (shrinkAmount / shrinkDuration) * Time.deltaTime);
             if (transform.localScale.x == targetDiameter)
                 shrinking = false;
         }
@@ -49,9 +52,19 @@ public class ForceField : MonoBehaviour
             targetDiameter -= shrinkAmount;
         else
             targetDiameter = minShrinkAmount;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            targetCenterLocation = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            photonView.RPC("SetOthercCenterLocation", RpcTarget.Others, targetCenterLocation);
+        }
         lastShrinkEndTime = Time.time + shrinkAmount;
     }
 
+    [PunRPC]
+    void SetOthercCenterLocation(Vector3 target)
+    {
+        targetCenterLocation = target;
+    }
     void CheckPlayers()
     {
         if (Time.time - lastPlayerCheckTime > 1.0f)
@@ -63,7 +76,7 @@ public class ForceField : MonoBehaviour
             {
                 if (player.dead || !player)
                     continue;
-                if (Vector3.Distance(Vector3.zero, player.transform.position) >= transform.localScale.x)
+                if (Vector3.Distance(this.transform.position, player.transform.position) >= transform.localScale.x)
                 {
                     player.photonView.RPC("TakeDamage", player.photonPlayer, 0, playerDamage);
                 }
